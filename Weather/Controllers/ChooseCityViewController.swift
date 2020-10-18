faa78b719a765325089a09f7fd7dfa44303068cb
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-18 20:56:17 -0300

    feature under construction(UI, ChooseCityVC, DetailVC)

---
+++ Weather/Controllers/ChooseCityViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChooseCityViewController.swift
+//  Weather
+//
+//  Created by Ezequiel Parada Beltran on 18/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ChooseCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
