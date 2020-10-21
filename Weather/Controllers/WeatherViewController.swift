54092446380375b30449d5d4869a54d496aaff3c
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-21 15:19:49 -0300

    feature under construction (Weather View)

--- Weather/Controllers/WeatherViewController.swift
+++ Weather/Controllers/WeatherViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
+    //MARK: - IBOutlets
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var weatherInfoLabel: UILabel!
     
+    //MARK: - Vars
+    var userDefaults = UserDefaults.standard
+    var allLocations: [WeatherLocation] = []
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocationCoordinate2D!
     
-
+//MARK: - LifeCycle view
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,7 +35,75 @@ class WeatherViewController: UIViewController {
         }
         
     }
+    
+    //MARK: - Location Manager
+    private func locationManagerStart(){
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager!.requestWhenInUseAuthorization()
+            locationManager!.delegate = self
+            
+        }
+        locationManager!.startMonitoringSignificantLocationChanges()
+    }
+    private func locationManagerStop(){
+        if locationManager != nil {
+            locationManager!.stopMonitoringSignificantLocationChanges()
+        }
+    }
+    private func locationAutoCheck(){
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            currentLocation = locationManager!.location?.coordinate
+            if currentLocation != nil {
+                LocationService.shared.latitude = currentLocation.latitude
+                LocationService.shared.longitude = currentLocation.longitude
+            } else {
+                locationAutoCheck()
+            }
+        } else {
+            locationManager?.requestWhenInUseAuthorization()
+            locationAutoCheck()
+        }
+    }
+    
+    //MARK: - DownloadWeather
+    private func getWeather() {
+        loadLocationsFromUserDefaults()
+    }
+    
+    //MARK: - UserDefaults
+    
+    private func loadLocationsFromUserDefaults(){
+        let currentLocation = WeatherLocation(city: "", country: "", countryCode: "", isCurrentLocation: true)
+        
+        if let data = userDefaults.value(forKey: "Locations") as? Data {
+            allLocations = try! PropertyListDecoder().decode(Array<WeatherLocation>.self, from: data)
+        } else {
+            print("No data in userDefaults")
+            allLocations.append(currentLocation)
+        }
+        
+    }
+    
+
+    //MARK: - Download Data
+    
+    private func getCurrentWeather(location: WeatherLocation) {
+        for i in 0..<allLocations.count {
+            
+            if i == 0 {
+                <#code#>
+            }
+        }
+    }
 
 
 }
 
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get location, \(error.localizedDescription)")
+    }
+}
---
+++ Weather/View/WeatherView/WeatherView.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Ezequiel Parada Beltran on 21/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
