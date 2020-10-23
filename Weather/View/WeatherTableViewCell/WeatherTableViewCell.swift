8faa95f4a1c7f9aa2090cb7418d3bb52b123980a
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-22 22:00:15 -0300

    building (custom TableViewCell)

---
+++ Weather/View/WeatherTableViewCell/WeatherTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherTableViewCell.swift
+//  Weather
+//
+//  Created by Ezequiel Parada Beltran on 22/10/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var dayOfTheWeekLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func generateCell(forecast: WeeklyWeatherForecast) {
+        dayOfTheWeekLabel.text = forecast.date.dayOfTheWeek()
+        weatherIconImageView.image = getWeatherIconFor(forecast.weatherIcon)
+        tempLabel.text = "\(forecast.temp)"
+    }
+    
+}
