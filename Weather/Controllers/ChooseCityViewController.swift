a8f5b0871b8c6671002388516b50e3182bf5cd97
ezequiel3434 <ezequiel3434@gmail.com>
2020-10-26 17:24:38 -0300

    building tableView datasource and delegate methods and rendering filtering data from searchbar textfield

--- Weather/Controllers/ChooseCityViewController.swift
+++ Weather/Controllers/ChooseCityViewController.swift
@@ -23,11 +23,19 @@ class ChooseCityViewController: UIViewController {
     //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.tableFooterView = UIView()
         setupSearchController()
         tableView.tableHeaderView = searchController.searchBar
         setupGesture()
+        loadLocationsFromCSV()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadFromUserDefaults()
     }
     
     
@@ -68,11 +76,43 @@ class ChooseCityViewController: UIViewController {
     //MARK: - Get Locations
     private func loadLocationsFromCSV(){
         if let path = Bundle.main.path(forResource: "location", ofType: ".csv") {
-            <#statements#>
+            parseCSVAt(url: URL(fileURLWithPath: path))
         }
     }
     
     private func parseCSVAt(url: URL){
+        do {
+            let data = try Data(contentsOf: url)
+            let dataEncoded = String(data: data, encoding: .utf8)
+            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({$0.components(separatedBy: ",")}){
+                var i = 0
+                for line in dataArr {
+                    if line.count > 2 && i != 0 {
+                        createLocation(line: line)
+                    }
+                    i += 1
+                }
+                
+            }
+            
+        } catch {
+            print("Error reading CSV file, ", error.localizedDescription)
+        }
+    }
+    
+    private func createLocation(line: [String]){
+        allLocations.append(WeatherLocation(city: line.first!, country: line.last!, countryCode: line[1], isCurrentLocation: false))
+    }
+    
+    
+    //MARK: - UserDefaults
+    private func loadFromUserDefaults(){
+        if let data = userDefaults.value(forKey: "Locations") as? Data {
+            savedLocations = try? PropertyListDecoder().decode(Array<WeatherLocation>.self, from: data)
+        }
+    }
+    
+    private func saveLocations(location: WeatherLocation){
         
     }
 
@@ -80,17 +120,39 @@ class ChooseCityViewController: UIViewController {
 
 }
 
+
+
 //MARK: - UISearchResultsUpdating
 extension ChooseCityViewController: UISearchResultsUpdating {
     private func filterContentForSearchText(searchText: String, scope: String = "All"){
+        
         filteredLocations = allLocations.filter({ (location) -> Bool in
+            
             return location.city.lowercased().contains(searchText.lowercased()) ||
                 location.country.lowercased().contains(searchText.lowercased())
         })
+        tableView.reloadData()
     }
     
     func updateSearchResults(for searchController: UISearchController) {
         filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    
+    
+}
+
+//MARK: - UITableViewDelegate, UITableViewDataSource
+extension ChooseCityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredLocations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let location = filteredLocations[indexPath.row]
+        cell.textLabel?.text = location.city
+        cell.detailTextLabel?.text = location.country
+        return cell
     }
     
     
